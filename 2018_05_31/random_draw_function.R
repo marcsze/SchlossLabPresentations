daa8bfaf2ec9ebc34767fef6a0868cc3bb7fa260
marcsze <marcasze@gmail.com>
2018-05-31 10:42:37 -0400

    update random draw code

--- 2018_05_31/random_draw_function.R
+++ 2018_05_31/random_draw_function.R
@@ -6,12 +6,14 @@ library(tidyverse)
 
 
 random_draw <- function(groups){
-  
+ 
   number_of_groups <- paste("g_", 1:groups, sep = "")
   
   a <- "blank"
   
   for(sampling_round in 1:length(number_of_groups)){
+    
+    Sys.sleep(5)
     
     if(sampling_round == 1){
       
@@ -25,10 +27,19 @@ random_draw <- function(groups){
     choice <- sample(temp_groups, size = 1)
     
     
+    if(sampling_round == 1){
+      
+      print(paste("Congratulations ", choice, " you get to choose first!", sep = ""))
+    } else if(sampling_round != length(number_of_groups)){
+      
+        print(paste(choice, " you get to choose next.", sep = ""))
+    } else{
+      
+      print(paste("Unfortunately, ", choice, " you get to choose last.", sep = ""))
+    }
     
-    print(choice)
     
-    Sys.sleep(3)
+    
   }
   
 }
